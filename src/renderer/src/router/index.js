50099a31815cdbdcb01b4ab2a9cf5d5fa1474e74
Philip Nilsson <philip.nilsson23@gmail.com>
2024-10-08 12:37:59 +0200

    npm audit fix to remove security vulnerabilities.

--- src/renderer/src/router/index.js
+++ src/renderer/src/router/index.js
@@ -16,14 +16,6 @@ const routes = [
     component: () => import(/* webpackChunkName: "settings" */ '../views/SettingsView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
     path: '/plipassist',
     name: 'plipassist',
     // route level code-splitting
